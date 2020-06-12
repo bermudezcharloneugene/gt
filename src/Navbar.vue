5f1781b8a0de99ecde959e2b5864ecb3b6225400
Eugene <bermudezcharloneugene@yahoo.com>
2020-06-12 22:44:02 +0800

    Modify navbar structure and styling

--- src/Navbar.vue
+++ src/Navbar.vue
@@ -1,33 +1,67 @@
 <template>
-    <div>
-      <b-navbar toggleable="lg" type="light" variant="light" fixed>
-        <b-navbar-brand href="#">EB</b-navbar-brand>
-    
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-    
-        <b-collapse id="nav-collapse" is-nav >
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item href="/">Home</b-nav-item>
-            <b-nav-item href="/information" >Information</b-nav-item>
-          </b-navbar-nav>
-    
-          <!-- Right aligned nav items -->
-          <!-- <b-navbar-nav class="ml-auto">
-            <b-nav-form>
-              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form>
-    
-    
-            <b-nav-item-dropdown right>
-              <template v-slot:button-content>
-                <em>User</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav> -->
-        </b-collapse>
-      </b-navbar>
-    </div>
+  <div >
+  <nav class="nav justify-content-center">
+    <a class="nav-link active" href="#about">About</a>
+    <a class="nav-link disabled" href="">Disabled link</a>
+  </nav>
+  </div>
 </template>
+
+<script>
+export default {
+	name: 'Navbar',
+	data() {
+		return {
+			navColor: 'navbar-dark bg-transparent',
+		}
+	},
+  created () {
+    window.addEventListener('scroll', this.calcScroll);
+  },
+  destroyed  () {
+    window.removeEventListener('scroll', this.calcScroll);
+  },
+  methods: {
+    calcScroll() {
+			if(window.scrollY >= 200) 
+			{
+				this.navColor = 'navbar-dark bg-dark';
+			}else {
+				this.navColor = 'navbar-dark bg-transparent';
+			}
+    }
+  }
+}
+</script>
+
+<style scoped>
+		.nav-text {
+			font-size: 16px;
+			color: white;
+		}
+		.nav-text:hover {
+			background: white;
+			color: black;
+			padding: .3em;
+			border-radius: 20%;
+		}
+		.navbar-brand {
+			text-shadow: 0px 0px 3px black;
+		}
+		.navbar-brand:hover {
+			text-shadow: 0px 0px 3px black;
+			background: rgba(145, 143, 143, 0.521);
+			padding: .2em;
+			border-top-left-radius: 30%;
+			border-bottom-right-radius: 30%;
+		}
+		.bg-dark {
+			transition: .8s;
+			background-color: rgb(0, 0, 0)!important;
+		}
+
+		.bg-transparent {
+			transition: .8s;
+			background-color: rgba(0,0,0,0)!important;
+		}
+</style>
